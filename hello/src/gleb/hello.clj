e900fcc247ace383444e715c047cb3742e97e2d4
glebmark <87950237+glebmark@users.noreply.github.com>
2026-01-31 16:50:38 +0400

    more basics

--- hello/src/gleb/hello.clj
+++ hello/src/gleb/hello.clj
@@ -10,3 +10,26 @@
   "I don't do a whole lot ... yet."
   [& args]
   (greet {:name (first args)}))
+
+(defn -summ
+  []
+  (+ 1 2 4))
+
+(if true
+  (+ 2 5)
+"By Aquaman's trident!")
+
+(def ^:private zeus-hammer
+  "By Zeus's hammer!")
+
+(defn -ifbranching
+  []
+  (if false
+  (do (println "Success!")
+      zeus-hammer)
+  (do (println "Failure!")
+      "By Aquaman's trident!")))
+
+(if false
+  "That won't be returned")
+; => nil
